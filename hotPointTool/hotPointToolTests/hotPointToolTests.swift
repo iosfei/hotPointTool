30c17908a0f86edf30f1827ed4a66a74d4fc0168
oileafly <oileafly@gmail.com>
2017-01-10 15:36:43 +0800

    转义xml

--- hotPointTool/hotPointToolTests/hotPointToolTests.swift
+++ hotPointTool/hotPointToolTests/hotPointToolTests.swift
@@ -27,18 +27,34 @@ class hotPointToolTests: XCTestCase {
         
         do{
 
-            var newStrPath =  "/Users/keney/Desktop/地块005.xml"
+            // 获取待处理xml 路径和文本
+            let newStrPath =  "/Users/keney/Desktop/地块005.xml"
             var str = try String.init(contentsOfFile: newStrPath, encoding: String.Encoding.isoLatin2)
             
+            // 移除头部信息
             str.removeSubrange(str.index(str.startIndex, offsetBy: 0)...str.index(str.startIndex, offsetBy: 39))
+            
+            // 保存 frame 的数据
             let strFrame = str.data(using: String.Encoding.isoLatin2)!
             
-  
+            // 模版头部信息
             let modelPath =  "/Users/keney/Desktop/hotspotdatafile.xml"
             let modelStr = try  String.init(contentsOfFile: modelPath).data(using: String.Encoding.isoLatin2)!
             
+            
+            print(try  String.init(contentsOfFile: modelPath))
+            // 拼接 模版头部 和 frame 的数据
             let newData = modelStr + strFrame
+            
+            
+            
+            let newStr =   "<hotspot bounds=\"{{0., 0.}, {50.0, 50.0}}\"" + " anchorPoint=\"{.5, .928}\"" +  " originSize=\"{2048.0, 1536.0}\"" +  " center=\"{0., 0.}\">" + "\n" +
+            
+                            "<backgroundImage state=\"0\" value=\"images/icon.png\" />"
+            
+            print("-\(newStr)")
 
+            // 写入 覆盖
             let fm = FileHandle.init(forWritingAtPath: newStrPath)
             fm?.seek(toFileOffset: 0)
             fm?.write(newData)

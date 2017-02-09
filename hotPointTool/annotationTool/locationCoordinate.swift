db728e9559555eeef55da4c500f9d4143dd73e8d
oileafly <oileafly@gmail.com>
2017-02-09 16:34:01 +0800

    创建对应的appreance.xml, appconfig.xml 文件

--- hotPointTool/annotationTool/locationCoordinate.swift
+++ hotPointTool/annotationTool/locationCoordinate.swift
@@ -10,12 +10,16 @@ import Cocoa
 
 class locationCoordinate: NSObject {
     
-   
+    lazy var fm: FileManager = {
+        
+        return FileManager.default
+    }()
+    
     override init() {
 
         super.init()
         
-        let stream = InputStream(fileAtPath: "/Users/keney/Downloads/地图_20170117135705.csv")!
+        let stream = InputStream(fileAtPath: "/Users/keney/Downloads/地图_20170209151544.csv")!
         
         var rowAry: [CSV.Element] = []
         
@@ -25,7 +29,7 @@ class locationCoordinate: NSObject {
             
         }
         
-        // 分离列数据和行数据
+        // 分离第一行id数据和其他行数据
         var idAry : [String] = []
         var newRowAry : [CSV.Element] = []
         for (index, _) in rowAry.enumerated(){
@@ -76,12 +80,37 @@ class locationCoordinate: NSObject {
         
         for (index, _) in loAry.enumerated() {
             
-            print(loAry[index].name, loAry[index].lat, loAry[index].lon, loAry[index].address, loAry[index].createtime, loAry[index].id, loAry[index].createtime)
+            print(loAry[index].name, loAry[index].lat, loAry[index].lon, loAry[index].address, loAry[index].id, loAry[index].createtime, loAry[index].updatetime)
+         
+            let objectPath = "/Users/keney/Desktop/" + "\(loAry[index].name)"
+           
+            do {
+                // 模块单元创建
+               try fm.createDirectory(atPath: objectPath, withIntermediateDirectories: true, attributes: nil)
+                
+                try fm.createDirectory(at: URL.init(fileURLWithPath: objectPath), withIntermediateDirectories: true, attributes: nil)
+                // appearance.xml 创建
+                let appearance = "<root contentBackgroundTintColor=\"#88e5b02c\" textTintColor=\"#ffffff\" barTintColor=\"#cc6fbe54\">" + "\n" +
+                    "<baseAppearance tintColor=\"#cc6fbe54\" borderColor=\"#cc6fbe54\" borderWidth=\"1.\" />" + "\n" +
+                "</root>"
+                
+                
+                let appearancePath = objectPath + "/appearance.xml"
+                
+               try fm.createFile(atPath: appearancePath, contents: appearance.data(using: String.Encoding.utf8), attributes: nil)
+                
+                // config.xml 创建
+                let configPath = objectPath + "/config.xml"
+                let config = "<root locationCoordinate=\"{\(loAry[index].lat),\(loAry[index].lon)}\"><type>MapAnnotation</type><layout>layout.xml</layout><name>\(loAry[index].name)</name><dataFile>datafile.xml</dataFile><icon>logo.png</icon><appearance>appearance.xml</appearance><subname></subname><resource>resources/wlwqdjd.htm</resource></root>"
+                
+                try fm.createFile(atPath: configPath, contents: config.data(using: String.Encoding.utf8), attributes: nil)
+                
+            } catch let error {
+                print(error)
+            }
             
         }
-            print("loAry==========================\(loAry)")
-
+        
     }
 
 }
-
